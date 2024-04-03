3c9dc8cf6282e7e5f5118fb6aa73101a8872d92e
Vallake <schmidt.benjamin@orange.fr>
2024-04-03 22:19:49 +0200

    liasion bdd + correction erreur syntaxe

--- deuxieme.php
+++ deuxieme.php
@@ -1,35 +1,46 @@
-<h1>Il y a tout nos contact</h1>
 
-
-
-
-<div class="card">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+    <body>
     
-    <div class="card-body">
-        <p class="card-text">nom : <?php echo $_POST['nom']; ?></p>
-        <p class="card-text">prenom : <?php echo $_POST['prenom']; ?></p>
-        <p class="card-text">age : <?php echo $_POST['age']; ?></p>
-        <p class="card-text">email : <?php echo $_POST['Email']; ?></p>
+
+        <div class="card-body">
+            <p >nom : <?php echo $_POST['nom']; ?></p>
+            <p >prenom : <?php echo $_POST['prenom']; ?></p>
+            <p >telephone: <?php echo $_POST['telephone']; ?></p>
+            <p >email : <?php echo $_POST['email']; ?></p>
+    
+        </div>
+    </body>
+</html>
+
+
         
 
 
  
         <?php
         try{
-            $mysqlClient = new PDO("mysql:host=localhost;dbname=redirection;charest=utf8","root","");
+            $mysqlClient = new PDO("mysql:host=localhost;dbname=ben;charest=utf8","root","");
         }
         catch (Exeption $e)
         {
             die("Erreur : ".$e->getMessage());
         }
-        $sqlQuery = "INSERT INTO maquette_html(nom,prenom,email) values (:nom, :prenom, :email)";
+        $sqlQuery = "INSERT INTO formulaire(nom,prenom,email,telephone) values (:nom, :prenom, :email, :telephone)";
 
         $insertRecipe = $mysqlClient->prepare($sqlQuery);
 
         $insertRecipe->execute([
             'nom' => $_POST['nom'],
             'prenom' => $_POST['prenom'],
-            'email' => $_POST['Email'],
+            'email' => $_POST['email'],
+            'telephone'=> $_POST['telephone'],
         ]);
 
         
